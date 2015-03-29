b36e66b153e300a53c21cb2c728f8accf3000823
victor.r@ukr.net <8102977aa>
2015-03-29 22:31:15 +0300

    Trying rubymine 7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.order("published_at")
   end
 
   # GET /articles/1
